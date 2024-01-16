b098f2f409d5215242dd76bb3b8f58fe7b18c3f3
Nikolay Shirokov <nshirokov@internet.ru>
2024-01-16 21:03:55 +0700

    add gitignore and DB

--- app.py
+++ app.py
@@ -3,44 +3,64 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import shortener
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///date_base.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///list.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 shortened_urls = {}
 
-class Date_Base(db.Model):
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), nullable=False)
+    #user_name = db.Column(db.String(50), nullable=False)
     full_url = db.Column(db.String, nullable=False)
-    short_url = db.Column(db.String(), nullable=False)
+    short_url = db.Column(db.String, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Date_Base %r>' % self.id
+        return '<Article %r>' % self.id
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        long_url = request.form['long_url']
+        full_url = request.form['full_url']
         short_url = shortener.generate_short_url()
         while short_url in shortened_urls:
             short_url = shortener.generate_short_url()
 
-        shortened_urls[short_url] = long_url
+        shortened_urls[short_url] = full_url
 
-        return render_template('short_url.html', short_url = short_url)
+        article = Article(full_url = full_url, short_url = short_url)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('short_url')
+        except:
+            return 'При преобразовании возникла проблема'
+
     return render_template('index.html')
 
 
+@app.route('/short_url')
+def short_url():
+    article = Article.query.order_by(Article.date).all()
+    return render_template('short_url.html', article = article)
+
 @app.route('/<short_url>')
 def redirect_url(short_url):
-    long_url = shortened_urls.get(short_url)
-    if long_url:
-        return redirect(long_url)
-    else:
-        return 'Url not found'
+    try:
+        f = Article.query.filter_by(short_url = short_url).one()
+        if f.full_url:
+            return redirect(f.full_url)
+    except:
+            return 'Url not found'
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():         # <--- without these two lines,
+        db.create_all()             # <--- we get the OperationalError in the title
+        app.run(debug=True)
